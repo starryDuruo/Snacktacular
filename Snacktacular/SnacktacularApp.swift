0185a096b87710c4b3babac7c238ff6ebdc6f94b
Sige Wang <wangfiz@bc.edu>
2026-03-26 15:53:49 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Wang Sige on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
